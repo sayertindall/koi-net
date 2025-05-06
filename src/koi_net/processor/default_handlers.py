0540d35a00c6d55829fb5a663887c55b18b81217
lukvmil <millerluke1364@gmail.com>
2025-05-06 21:35:49 +0800

    added coordinator contact handler to default handlers, this behavior has been replicated across all existing nodes, so worth making a new default

--- src/koi_net/processor/default_handlers.py
+++ src/koi_net/processor/default_handlers.py
@@ -9,6 +9,8 @@ from .handler import KnowledgeHandler, HandlerType, STOP_CHAIN
 from .knowledge_object import KnowledgeObject, KnowledgeSource
 from ..protocol.event import Event, EventType
 from ..protocol.edge import EdgeProfile, EdgeStatus, EdgeType
+from ..protocol.node import NodeProfile
+from ..protocol.helpers import generate_edge_bundle
 
 logger = logging.getLogger(__name__)
 
@@ -123,6 +125,57 @@ def edge_negotiation_handler(processor: ProcessorInterface, kobj: KnowledgeObjec
 
 
 # Network handlers
+
+@KnowledgeHandler.create(HandlerType.Network, rid_types=[KoiNetNode])
+def coordinator_contact(processor: ProcessorInterface, kobj: KnowledgeObject):
+    node_profile = kobj.bundle.validate_contents(NodeProfile)
+    
+    # looking for event provider of nodes
+    if KoiNetNode not in node_profile.provides.event:
+        return
+    
+    # already have an edge established
+    if processor.network.graph.get_edge_profile(
+        source=kobj.rid,
+        target=processor.identity.rid,
+    ) is not None:
+        return
+    
+    logger.info("Identified a coordinator!")
+    logger.info("Proposing new edge")
+    
+    if processor.identity.profile.node_type == NodeType.FULL:
+        edge_type = EdgeType.WEBHOOK
+    else:
+        edge_type = EdgeType.POLL
+    
+    # queued for processing
+    processor.handle(bundle=generate_edge_bundle(
+        source=kobj.rid,
+        target=processor.identity.rid,
+        edge_type=edge_type,
+        rid_types=[KoiNetNode]
+    ))
+    
+    logger.info("Catching up on network state")
+    
+    payload = processor.network.request_handler.fetch_rids(
+        node=kobj.rid, 
+        rid_types=[KoiNetNode]
+    )
+    for rid in payload.rids:
+        if rid == processor.identity.rid:
+            logger.info("Skipping myself")
+            continue
+        if processor.cache.exists(rid):
+            logger.info(f"Skipping known RID '{rid}'")
+            continue
+        
+        # marked as external since we are handling RIDs from another node
+        # will fetch remotely instead of checking local cache
+        processor.handle(rid=rid, source=KnowledgeSource.External)
+    logger.info("Done")
+    
 
 @KnowledgeHandler.create(HandlerType.Network)
 def basic_network_output_filter(processor: ProcessorInterface, kobj: KnowledgeObject):
